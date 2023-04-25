3c2ae9f3557622b0932d4becdda5369f1769fa46
ヒガシ <125252644+Higashi8910@users.noreply.github.com>
2023-04-25 07:36:34 +0000

    Update TodoGraph.jsx

--- src/TodoGraph.jsx
+++ src/TodoGraph.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Chart from "react-apexcharts";
 
 const TodoGraph = ({ completeTodos, incompleteTodos }) => {
-  console.log("completeTodos:", completeTodos);
-  console.log("incompleteTodos:", incompleteTodos);
   const priorityCounts = {
     high: { complete: 0, incomplete: 0 },
     medium: { complete: 0, incomplete: 0 },
@@ -11,17 +9,6 @@ const TodoGraph = ({ completeTodos, incompleteTodos }) => {
     other: { complete: 0, incomplete: 0 },
   };
 
-  // completeTodos.forEach((todo) => {
-  //   if (todo.priority && priorityCounts[todo.priority]) {
-  //     priorityCounts[todo.priority].complete += 1;
-  //   }
-  // });
-
-  // incompleteTodos.forEach((todo) => {
-  //   if (todo.priority && priorityCounts[todo.priority]) {
-  //     priorityCounts[todo.priority].incomplete += 1;
-  //   }
-  // });
   completeTodos.forEach((todo) => {
     if (todo.priority === "大") {
       priorityCounts.high.complete++;
@@ -45,8 +32,6 @@ const TodoGraph = ({ completeTodos, incompleteTodos }) => {
       priorityCounts.other.incomplete++;
     }
   });
-
-  console.log("priorityCounts:", priorityCounts);
 
   const chartData = {
     series: [
